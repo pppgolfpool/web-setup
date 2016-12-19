07e3e97f15d55a65b19e56d2433427f1cdb22c54
Michael Daw <michael.k.daw@gmail.com>
2016-12-18 18:41:45 -0700

    work

---
+++ src/app.js
@@ -0,0 +1,43 @@
+import { inject } from "aurelia-framework";
+import { HttpClient } from "aurelia-fetch-client";
+
+@inject(HttpClient)
+export class App {
+  constructor(http) {
+    this.http = http;
+    this.email = '';
+    this.password = '';
+    this.confirm = '';
+  }
+
+  activate(params) {
+    var queryDict = {};
+    if(location.search){
+      location.search.substr(1).split("&").forEach(function(item) {queryDict[item.split("=")[0]] = item.split("=")[1]})
+      console.log("logging query");
+      console.log(queryDict);
+      this.authUrl = queryDict['authUrl'];
+      this.redirect = queryDict['redirect'];
+      this.email = queryDict['userId'];
+      this.code = queryDict['code'];
+    } else {
+      
+    }
+  }
+
+  do() {
+    if(!this.email || !this.password || !this.confirm){
+      return;
+    }
+    if(this.password !== this.confirm){
+      return;
+    }
+    
+    this.http.fetch(`${this.authUrl}/changepassword?userId=${this.email}&changeCode=${this.code}&newPassword=${this.confirm}`).
+      then(
+      response => response.json().then(data => {
+        window.location.replace(this.redirect)
+      }),
+      response => response => alert('unauthorized'));
+  }
+}
